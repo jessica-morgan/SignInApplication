3f057b44303b27435654a911e8c906cd0431e76d
jess morgan <jess-morgan1@outlook.com>
2019-08-29 16:12:56 +1200

    main page to sign in

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import { Route } from 'react-router-dom'
-import MainPage from './MainPage'
+// import MainPage from './MainPage'
 import SignInForm from './SignInForm'
 import SignOutForm from './SignOutForm'
 
 const App = () => {
   return (
     <div>
-      <Route exact path="/" component={MainPage} />
-      <Route path="/signin" component={SignInForm} />
+      <Route exact path="/" component={SignInForm} />
+      {/* <Route path="/signin" component={SignInForm} /> */}
       <Route path="/signout" component={SignOutForm} />
     </div>
   )
