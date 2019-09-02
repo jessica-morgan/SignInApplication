2323ac5379b7877486c0640148468ad29ab4eaba
jess morgan <jess-morgan1@outlook.com>
2019-09-03 10:09:58 +1200

    signout form

--- client/components/SignOutForm.jsx
+++ client/components/SignOutForm.jsx
@@ -1,40 +1,38 @@
 import React, { useState } from 'react'
-import TextField from '@material-ui/core/TextField'
 import Button from '@material-ui/core/Button'
-
-import { styles } from '../utils'
+import { CustomInput, signInButtonStyles } from '../utils'
 import BackArrow from './BackArrow'
 
 export default function SignOutForm () {
   const [email, setEmail] = useState('')
-
-  const style = styles()
+  const buttonStyle = signInButtonStyles()
 
   return (
-    <div className='page-background'>
-      <header>
-        <h1 className='page-text'>Thank you for visiting</h1>
-        <h1 style={{ paddingLeft: '10%' }}>Enspiral Dev Academy.</h1>
-      </header>
-      <br/>
-      <form style={{ marginLeft: '10%' }}>
-        <br/><br/><br/>
-        <TextField
-          placeholder='email'
-          style={{ paddingLeft: '6px' }}
-          name='email'
-          InputProps={{ disableUnderline: true }}
-          className='form-inputs'
-          type='text'
-          value={email}
-          onChange={e => setEmail(e.target.value)}
-          required
-        />
-      </form>
-      <br/>
-      {/* on click send new date object to get time of sign out */}
-      <Button className={style.root}>Sign out</Button>
-      <BackArrow />
+    <div className='page-background-half'>
+      <div className='sign-out-form-image'>
+        <header>
+          <h1 className='sign-out-page-text'>Thank you for visiting
+            <br/>Enspiral Dev Academy.</h1>
+        </header>
+        <br/>
+        <form style={{ marginLeft: '10%', marginBottom: '0vh' }}>
+          <br/><br/><br/>
+          <CustomInput
+            placeholder="email"
+            InputProps={{ disableUnderline: true }}
+            type='text'
+            name='email'
+            onChange={e => setEmail(e.target.value)}
+            required
+          />
+        </form>
+        <br/>
+        {/* on click send new date object to get time of sign out */}
+        <div style={{ marginBottom: '27vh', marginLeft: '5vw' }}>
+          <Button className={buttonStyle.root}>Sign out</Button>
+        </div>
+        <BackArrow />
+      </div>
     </div>
   )
 }
