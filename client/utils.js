ae6ac3375d99621ce09476981ad79ded632e79d8
jess morgan <jess-morgan1@outlook.com>
2019-09-03 10:09:08 +1200

    removed unused style function

--- client/utils.js
+++ client/utils.js
@@ -1,26 +1,6 @@
 import { makeStyles, withStyles, createMuiTheme } from '@material-ui/core/styles'
 import InputBase from '@material-ui/core/InputBase'
 
-const useStyles = makeStyles({
-  root: {
-    background: 'white',
-    border: 0,
-    borderRadius: 9,
-    color: '#2C97AF',
-    fontFamily: 'Roboto, sans-serif',
-    fontSize: '2vw',
-    fontWeight: 'bold',
-    textTransform: 'none',
-    marginLeft: '7vw',
-    height: '7vh',
-    width: '12vw',
-    '&:hover': {
-      backgroundColor: 'white',
-      color: 'rgb(27, 111, 129)'
-    }
-  }
-})
-
 const signInButtonStyles = makeStyles({
   root: {
     background: 'white',
@@ -31,7 +11,7 @@ const signInButtonStyles = makeStyles({
     fontSize: '1.5vw',
     fontWeight: 'bold',
     marginLeft: '30.5vw',
-    marginTop: '4vh',
+    marginTop: '-1vh',
     height: '5vh',
     width: '9vw',
     textTransform: 'none',
@@ -131,7 +111,6 @@ const selectStyle = createMuiTheme({
 })
 
 module.exports = {
-  useStyles: useStyles,
   signInButtonStyles: signInButtonStyles,
   CustomDropDown: CustomDropDown,
   CustomInput: CustomInput,
