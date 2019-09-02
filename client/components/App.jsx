47b86a81019009d5d22dc838ea32c642f078f94d
jess morgan <jess-morgan1@outlook.com>
2019-09-02 21:39:21 +1200

    added route for sign in success

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import { Route } from 'react-router-dom'
-// import MainPage from './MainPage'
 import SignInForm from './SignInForm'
 import SignOutForm from './SignOutForm'
+import SignInSuccess from './SignInSuccess'
 
 const App = () => {
   return (
     <div>
       <Route exact path="/" component={SignInForm} />
-      {/* <Route path="/signin" component={SignInForm} /> */}
       <Route path="/signout" component={SignOutForm} />
+      <Route path="/signinsuccess" component={SignInSuccess} />
     </div>
   )
 }
