fd5ec1d1598e0b50e54b21e0b213079f41e229bc
Emily Coco Parkes <emilycoco@me.com>
2019-10-15 14:31:35 +1300

    admin auth serverside

--- server/server.js
+++ server/server.js
@@ -1,10 +1,12 @@
 const path = require('path')
 const express = require('express')
 const server = express()
-// const router = require('./routes')
+
+const authRoutes = require('./auth')
+
 server.use(express.json())
 server.use(express.static(path.join(__dirname, '../public')))
 
-// server.use('/', router)
+server.use('/api/v1/auth', authRoutes)
 
 module.exports = server
