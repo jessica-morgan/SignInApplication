250640332792b9c7645e6e06a104703c52b07309
jess morgan <jess-morgan1@outlook.com>
2019-10-16 16:48:06 +1300

    sign in button rendeing conditionally

--- client/components/SignInForm.jsx
+++ client/components/SignInForm.jsx
@@ -6,6 +6,7 @@ import MenuItem from '@material-ui/core/MenuItem'
 import { MuiThemeProvider } from '@material-ui/core/styles'
 import { signInButtonStyles, CustomDropDown, CustomInput, menuItemStyle, selectStyle } from '../utils'
 import { Link } from 'react-router-dom'
+import { newVisitorApi } from '../api'
 
 export default function SignInForm () {
   // Using hooks we are no longer declaring a single object called state that holds our component’s state. Instead,
@@ -16,10 +17,9 @@ export default function SignInForm () {
   // the name of the state variable we’re modifying.
   // We’re basically declaring a state variable and a function to allow us to modify the state variable later.
   // The empty string in the useState call is the initial value of fullName and can be set to any required value.
-  const [fullName, setFullName] = useState('name')
-  const [email, setEmail] = useState('email')
-  const [reason, setReason] = useState('reason')
-  const [signInTime, setSignInTime] = useState('signInTime')
+  const [fullName, setFullName] = useState('')
+  const [email, setEmail] = useState('')
+  const [reason, setReason] = useState('')
   const buttonStyle = signInButtonStyles()
   const menuStyles = menuItemStyle()
 
@@ -39,7 +39,6 @@ export default function SignInForm () {
             InputProps={{ disableUnderline: true }}
             type='text'
             name='fullName'
-            // dont need to write event handler functions when using hooks
             onChange={e => setFullName(e.target.value)}
             required
           />
@@ -74,9 +73,10 @@ export default function SignInForm () {
             </MuiThemeProvider>
           </FormControl>
           <br/>
-          {/* should link to /signinsuccess provided all fields have been filled */}
-          {/* send new date object onClick along with fullName, email and reason */}
-          <Button className={buttonStyle.root}>Sign in</Button>
+          {fullName.length && email.length && reason.length
+            ? <Link style={{ textDecoration: 'none' }} to='/signinsuccess'><Button className={buttonStyle.root} onClick={() => newVisitorApi(fullName, email, reason) }>Sign in</Button></Link>
+            : <div style={{ height: '9vh' }}></div>
+          }
         </form>
         <Link style={{ textDecoration: 'none' }} to='/signout'>
           <h3 className='sign-out-text'>or sign out instead</h3>
