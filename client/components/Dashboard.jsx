e32649129ea44b3e55fa09ad4fae77884aff7059
jess morgan <jess-morgan1@outlook.com>
2019-12-05 11:44:40 +1300

    condition

--- client/components/Dashboard.jsx
+++ client/components/Dashboard.jsx
@@ -78,7 +78,7 @@ export default function Dashboard () {
               </ListItem>
 
             </div>
-          } else {
+          } else if (visitor.signOutTime === null) {
             return <div key={visitor.id}>
               <ListItem alignItems="flex-start">
                 <ListItemAvatar>
